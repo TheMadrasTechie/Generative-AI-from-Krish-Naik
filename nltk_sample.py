4c1a23a6699dd7d76a00005e126044e3bcdb4b95
TheMadrasTechie <techiemadras@gmail.com>
2025-07-05 10:16:21 +0530

    lemmitization

--- nltk_sample.py
+++ nltk_sample.py
@@ -1,13 +1,16 @@
-import os
+# import os
+# import nltk
+# from nltk.tokenize import sent_tokenize
+
+# # Ensure current dir nltk_data is added
+# #nltk.data.path.append(os.path.join(os.getcwd(), 'nltk_data'))
+
+# # Example corpus
+# corpus = "Your text here. Another sentence! One more?"
+
+# # Sentence tokenization
+# sentences = sent_tokenize(corpus)
+# print(sentences)
+
 import nltk
-from nltk.tokenize import sent_tokenize
-
-# Ensure current dir nltk_data is added
-#nltk.data.path.append(os.path.join(os.getcwd(), 'nltk_data'))
-
-# Example corpus
-corpus = "Your text here. Another sentence! One more?"
-
-# Sentence tokenization
-sentences = sent_tokenize(corpus)
-print(sentences)
+nltk.download('wordnet')
