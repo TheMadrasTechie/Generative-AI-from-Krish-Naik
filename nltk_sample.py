0e7d2cfba206fbb1bcd4daaccea13da114e7eee9
TheMadrasTechie <techiemadras@gmail.com>
2025-07-05 09:20:00 +0530

    nlkt initialization

---
+++ nltk_sample.py
@@ -0,0 +1,13 @@
+import os
+import nltk
+from nltk.tokenize import sent_tokenize
+
+# Ensure current dir nltk_data is added
+#nltk.data.path.append(os.path.join(os.getcwd(), 'nltk_data'))
+
+# Example corpus
+corpus = "Your text here. Another sentence! One more?"
+
+# Sentence tokenization
+sentences = sent_tokenize(corpus)
+print(sentences)
