c9f30b69aef70d5365a4f73b4d315d31b3598b88
TheMadrasTechie <techiemadras@gmail.com>
2025-07-05 17:18:45 +0530

    parts of speech tagging

--- nltk_sample.py
+++ nltk_sample.py
@@ -1,16 +1,35 @@
-# import os
+# # import os
+# # import nltk
+# # from nltk.tokenize import sent_tokenize
+
+# # # Ensure current dir nltk_data is added
+# # #nltk.data.path.append(os.path.join(os.getcwd(), 'nltk_data'))
+
+# # # Example corpus
+# # corpus = "Your text here. Another sentence! One more?"
+
+# # # Sentence tokenization
+# # sentences = sent_tokenize(corpus)
+# # print(sentences)
+
 # import nltk
-# from nltk.tokenize import sent_tokenize
+# nltk.download('wordnet')
 
-# # Ensure current dir nltk_data is added
-# #nltk.data.path.append(os.path.join(os.getcwd(), 'nltk_data'))
+# import nltk
+# from nltk.corpus import stopwords
+# from nltk.tokenize import word_tokenize
 
-# # Example corpus
-# corpus = "Your text here. Another sentence! One more?"
+# # Download required resources
+# nltk.download('punkt')
+# nltk.download('averaged_perceptron_tagger')
+# nltk.download('stopwords')
 
-# # Sentence tokenization
-# sentences = sent_tokenize(corpus)
-# print(sentences)
+# text = "Shopify and Amazon both offer seller APIs for developers."
+# words = word_tokenize(text)
+# words = [word for word in words if word.lower() not in set(stopwords.words('english'))]
+# pos_tag = nltk.pos_tag(words)
+
+# print(pos_tag)
 
 import nltk
-nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger_eng')
